60a879596102d22e6acf226a3e57979a2f0ec82d
mattiasilvestri94 <mattia.silvestri94@gmail.com>
2019-10-08 20:16:33 +0200

    Updated Linear Regression

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 import pandas
 from ml_algorithms import LinearRegression
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
 
-# get training data and classes
-df = pandas.read_csv('data/train.csv')
+# get training data
+df = pandas.read_csv('data/train.csv', index_col='Id')
 
-
-y_train = df['SalePrice'].copy()
-features = ['LotArea', 'YearBuilt']
-x_train = df[features]
-x = x_train.values #returns a numpy array
+Y = df['SalePrice'].copy()
+features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = df[features]
+x = X.values #returns a numpy array
 min_max_scaler = preprocessing.MinMaxScaler()
 x_scaled = min_max_scaler.fit_transform(x)
-x_train = pandas.DataFrame(x_scaled)
+X = pandas.DataFrame(x_scaled)
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 # create and train the model
-model = LinearRegression(num_features=len(features))
-model.train(X=x_train, Y=y_train, epochs=10000)
+model = LinearRegression(num_features=len(features), lr=0.1)
+history = model.train(X=x_train, Y=y_train, epochs=2000)
+model.test(X=x_test, Y=y_test)
+
+history = history[100:]
+plt.plot(np.arange(0, len(history)), history)
+plt.show()
+
+# get training data
+df = pandas.read_csv('data/test.csv', index_col='Id')
+
+X = df[features]
+x = X.values #returns a numpy array
+min_max_scaler = preprocessing.MinMaxScaler()
+x_scaled = min_max_scaler.fit_transform(x)
+X = pandas.DataFrame(x_scaled)
+
+predictions = model.predict(X)
+# save test predictions to file
+output = pandas.DataFrame({'Id': df.index,
+                       'SalePrice': predictions.reshape(-1)})
+
+# file submission for Kaggle competition
+output.to_csv('submission.csv', index=False)
--- ml_algorithms.py
+++ ml_algorithms.py
@@ -1,16 +1,33 @@
 import numpy as np
-import pandas
 
 
 class LinearRegression:
 
-    def __init__(self, num_features, lr=0.01):
+    def __init__(self, num_features, lr=0.1):
+
+        '''
+        :param num_features: input dimension
+        :param lr: learning rate
+        '''
+
         self.n = num_features
         self.theta = np.random.normal(size=(self.n, ))
         self.learning_rate = lr
 
-    def train(self, X, Y, epochs=1000):
+
+    def train(self, X, Y, epochs=1000, batch_size=32):
+
+        '''
+
+        :param X: examples input as Pandas dataframe
+        :param Y: examples classes as Pandas dataframe
+        :param epochs: number of epochs
+        :param batch_size: batch size
+        :return: history of mae during training process
+        '''
+
         X, Y = X.to_numpy(), Y.to_numpy()
+        history = []
 
         for e in range(0, epochs):
             pred_error = 0
@@ -20,6 +37,45 @@ class LinearRegression:
                 pred_error += (predict - Y[i]) * X[i]
                 mae += abs(predict - Y[i])
 
-            loss = self.learning_rate * pred_error / X.shape[0]
-            self.theta -= loss
+                if (i + 1) % batch_size == 0:
+                    loss = self.learning_rate * pred_error / X.shape[0]
+                    self.theta -= loss
+
             print('Epoch: {} | MAE: {}'.format(e, mae))
+            history.append(mae)
+
+        return history
+
+    def test(self, X, Y):
+
+        '''
+
+        :param X: examples input as Pandas dataframe
+        :param Y: examples class as Pandas dataframe
+        :return:
+        '''
+
+        mae = 0
+        X, Y = X.to_numpy(), Y.to_numpy()
+
+        for i in range(0, X.shape[0]):
+            predict = np.dot(X[i].T, self.theta)
+            mae += abs(predict - Y[i])
+
+        print('Validation MAE: {}'.format(mae))
+
+    def predict(self, X):
+
+        '''
+        :param X: examples input as Pandas dataframe
+        :return: array of predictions
+        '''
+
+        X = X.to_numpy()
+        y = []
+
+        for i in range(0, X.shape[0]):
+            predict = np.dot(X[i].T, self.theta)
+            y.append(predict)
+
+        return np.asarray(y)
